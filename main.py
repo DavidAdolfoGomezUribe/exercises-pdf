e823d7960c5e6290fcbf26fb5446815e67056667
DavidAdolfoGomezUribe <96323893+DavidAdolfoGomezUribe@users.noreply.github.com>
2024-11-07 12:36:09 -0500

    solution exersice 1(corrected)

--- main.py
+++ main.py
@@ -6,17 +6,19 @@
 
 name = input("Hello,please enter your name:")
 while True:
-    print(f"Hello Mrs/Ms {name} this is a program for calculate if a number is even or odd.")
-    number = float(input("        Enter the number"))
-   
-    if number % 2 == 0:
-        print(f"        Your number {number} is even.")
-        break
-    else:
-        print(f"        Your number {number} is odd")
-        break
+    try:
+        print(f"Hello Mrs/Ms {name} this is a program for calculate if a number is even or odd.")
+        number = float(input("        Enter the number: "))
     
-    
+        if number % 2 == 0:
+            print(f"        Your number {number} is even.")
+            break
+        else:
+            print(f"        Your number {number} is odd")
+            break
+        
+    except:
+        print("        Please enter a valid number") 
     
     
     
