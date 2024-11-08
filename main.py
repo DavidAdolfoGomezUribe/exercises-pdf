f4e91b71f6b00dfde476ce9b60d15cdeb6cd51cf
DavidAdolfoGomezUribe <96323893+DavidAdolfoGomezUribe@users.noreply.github.com>
2024-11-07 21:24:30 -0500

    Solution for exercise 4

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-#Ejercicio 3: Calculadora básica
-#Utiliza match para implementar una calculadora simple.
-#Enunciado:
-#Crea una calculadora que solicite dos números y una operación matemática (+, -, *, /). Usa match
-#para realizar la operación correspondiente.
+#Ejercicio 4: Determinación del tipo de triángulo 
+#match 
+#Escribe un programa que determine el tipo de triángulo en función de sus lados usando 
+#if .
+# Enunciado: 
+#Solicita al usuario que ingrese las longitudes de los tres lados de un triángulo. Determina si el 
+#triángulo es equilátero, isósceles o escaleno.
 
 import pygame
 import math
@@ -18,36 +20,22 @@ while True:
     try:
         
         
-        print(f"""\nHello Mrs/Ms {name} this is a program for a basic calculator. Select the operation that you want todo (+,-,/,*) \n""")
+        print(f"""\nHello Mrs/Ms {name} this is a program for calculate a triangle type based on its sides \n""")
         
-        variable = str(input("        "))
-        match variable:
-            case "+":
-                a = float(input("       Enter the first number: "))
-                b = float(input("       Enter the second number: "))
-                total = a + b
-                print(f"        your total is {total} \n")
-            case "-":
-                a = float(input("       Enter the first number: "))
-                b = float(input("       Enter the second number: "))
-                total = a - b
-                print(f"        your total is {total} \n")
-            case "/":
-                a = float(input("       Enter the first number: "))
-                b = float(input("       Enter the second number: "))
-                    
-                if b != 0:
-                    total = a / b
-                    print(f"        your total is {total} \n")
-                
-                else :
-                    print("     The division by Zero is not possible ") 
-                
-            case "*":
-                a = float(input("       Enter the first number: "))
-                b = float(input("       Enter the second number: "))
-                total = a * b
-                print(f"       Your total is {total} \n")
+        sideA = float(input("        Enter the A side: "))
+        sideB = float(input("        Enter the B side: "))
+        sideC = float(input("        Enter the C side: "))
+
+        if not (sideA + sideB > sideC and sideC + sideB > sideA and sideA + sideC > sideB ):
+            raise ValueError
+
+        if sideA == sideB == sideC :
+            print ("\n        This is an equilateral triangle\n")    
+        if sideA == sideB or sideB == sideC or sideC == sideA: 
+            print ("\n        This is an isoceles triangle\n")
+        else:    
+            print ("\n        This is an escalene triangle\n")
+
 
         continueToAsk = input("    Do you want to calculate again?").strip().lower()
         
@@ -89,11 +77,51 @@ while True:
             input("")
             break
     except:
-        print("    Please enter a valid number") 
+        print("    Its not a valid triangle\n") 
+        continueToAsk = input("    Do you want to calculate again?").strip().lower()
+        
+        if continueToAsk == "yes" :
+            pass
+        else:
+            
+            
+            pygame.mixer.music.load("chad.mp3")
+            pygame.mixer.music.set_volume(0.65)
+            pygame.mixer.music.play()
+            
+            lines = [
+                "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⣀⣀⣠⣤⣤⣄⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+                "⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⠤⠖⠊⠉⠁⠀⠀ ⠀⠀⠀⠀⠀⠀⠉⠙⠲⢤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+                "⠀⠀⠀⠀⠀⠀⠀⡤⠊⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⠀⠀⠀⠀⠀⠀⠀⠈⢦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+                "⠀⠀⠀⠀⠀⠀⡜⠀⠀⠀⠀⠀⠀⢀⡀⠀⠀⠀⠀⠀⠀ ⠀⠀⠀⢢⠀⠀⠀⠀⢳⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+                "⠀⠀⠀⠀⠀⣸⠁⠀⠀⠀⠀⠀⠀⠀⠱⡀⠀⠀⠀⠀⠀⠀⠀⡀⠈⠀⡀⠀⠀⠀⠈⡇⠀⠀⠀⠀⠀⠀⠀⠀",
+                "⠀⠀⠀⠀⠀⡏⠀⠀⠀⠀⠀⠀⠀⠀⡰⠁⠀⠀⠀⠀⠀⠀⠀⠘⡆⡜⠁⠀⠀⠀⠀⢧⡀⠀⠀⠀⠀⠀⠀⠀",
+                "⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠸⡀⠀⠀⠀⠀⠀⣀⣤⡂⠀⠇⠱⠀⡀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀",
+                "⠀⠀⠀⠀⠀⢇⠀⠀⠀⠀⠀⠀⠀⠀⠈⢄⡀⢠⣟⢭⣥⣤⠽⡆⠀⡶⣊⣉⣲⣤⢀⡞⠀⠀⠀⠀⠀⠀⠀⠀",
+                "⠀⠀⠀⠀⠀⠘⣆⠀⠀⠀⠀⠀⠀⡀⠀⠐⠂⠘⠄⣈⣙⡡⡴⠀⠀⠙⣄⠙⣛⠜⠘⣆⠀⠀⠀⠀⠀⠀⠀⠀",
+                "⠀⠀⠀⠀⠀⠀⠈⢦⡀⠀⠀⠀⢸⠁⠀⠀⠀⠀⠀⠀⠄⠊⠀⠀⠀⠀⡸⠛⠀⠀⠀⢸⠆⠀⠀⠀⠀⠀⠀⠀",
+                "⠀⠀⠀⠀⠀⠀⠀⠀⠈⠓⠦⢄⣘⣄⠀⠀⠀⠀⠀⠀⠀⡠⠀⠀⠀⠀⣇⡀⠀⠀⣠⠎⠀⠀⠀⠀⠀⠀⠀⠀",
+                "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⠁⠈⡟⠒⠲⣄⠀⠀⡰⠇⠖⢄⠀⠀⡹⡇⢀⠎⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+                "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡇⠀⠀⡇⠀⠀⠹⠀⡞⠀⠀⢀⠤⣍⠭⡀⢱⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+                "⠀⠀⠀⠀⠀⠀⢀⣀⣀⣠⠞⠀⠀⢠⡇⠀⠀⠀⠀⠁⠀⢴⠥⠤⠦⠦⡼⠀⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+                "⣀⣤⣴⣶⣿⣿⡟⠁⠀⠋⠀⠀⠀⢸⠁⠀⠀⠀⠀⠀⠀⠀⠑⣠⢤⠐⠁⠀⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+                "⣿⣿⣿⣿⣿⡟⠀⠀⠀⠀⠀⠀⠀⢸⡀⠀⠀⠀⠀⠀⠀⠀⠀⠬⠥⣄⠀⠀⠈⠲⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+                "⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠙⠦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠈⢳⠀⠀⢀⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀",
+                "⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠒⠦⠤⢤⣄⣀⣠⠤⢿⣶⣶⣿⣿⣿⣶⣤⡀⠀⠀⠀⠀⠀",
+                "⣿⣿⣿⣿⣿⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡼⠁⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣄⠀⠀⠀⠀",
+                "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣦⣤⣤⣀⣀⣀⣀⣀⣀⣀⣤⣤⣤⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀"
+            ]
+                                          
+            for line in lines:
+                print(line)
+                time.sleep(0.41)
+            print("    Thanks for using the program")
+            
+            break
+ 
 
 
 
 
-
-#last line of code    
+  #last line of code    
     
